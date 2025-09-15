7976048a4fb17eff6819bbbc0feaed41fac36030
ALEEM ABDULMUHEEZ Ayomide <aleemabdulmuheez@gmail.com>
2025-09-15 11:38:49 +0100

    I have main.ts where it should be

---
+++ 0x04-TypeScript/task_1/dist/js/main.js
@@ -0,0 +1,35 @@
+var teacher3 = {
+    firstName: 'John',
+    lastName: 'Doe',
+    fullTimeEmployee: false,
+    location: 'London',
+    contract: false,
+};
+console.log(teacher3);
+var director1 = {
+    firstName: 'John',
+    lastName: 'Doe',
+    location: 'London',
+    fullTimeEmployee: true,
+    numberOfReports: 17,
+};
+console.log(director1);
+var printTeacher = function (firstName, lastName) { return "".concat(firstName.charAt(0), ". ").concat(lastName); };
+console.log(printTeacher('John', 'Doe')); // -> J. Doe
+var StudentClass = /** @class */ (function () {
+    function StudentClass(firstName, lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+    StudentClass.prototype.workOnHomework = function () {
+        return 'Currently working';
+    };
+    StudentClass.prototype.displayName = function () {
+        return this.firstName;
+    };
+    return StudentClass;
+}());
+var s = new StudentClass('John', 'Doe');
+console.log(s.displayName()); // John
+console.log(s.workOnHomework()); // Currently working
+//# sourceMappingURL=main.js.map
